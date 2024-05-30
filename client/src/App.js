c7028646c896294122df4b40200dbbe0a8bef194
Liam Boston <lvb5483@psu.edu>
2024-05-30 16:45:17 -0400

    Related to GAT-110 - created basic SignIn interface to test auth functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import SignIn from './components/auth/SignIn';
 import './App.css';
 
 class App extends React.Component {
@@ -21,13 +21,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>{this.state.serverResponse}</p>
-        </header>
+        <SignIn />
       </div>
     );
   }
