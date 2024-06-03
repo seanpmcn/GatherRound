3dcfede761f5b59c712811c10fad09b85e59d0d7
seanpmcn <46979553+seanpmcn@users.noreply.github.com>
2024-06-03 14:52:12 -0400

    GTR-111 Update firebase config

--- server/firestore.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-
-const { initializeApp, cert } = require("firebase-admin/app");
-const { getFirestore } = require("firebase-admin/firestore");
-
-const serviceAccount = require("./firestore-key.json");
