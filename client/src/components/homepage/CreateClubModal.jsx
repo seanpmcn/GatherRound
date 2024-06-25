d4de758a07d1d97db19aed304854349f4794df24
Liam Boston <lvb5483@psu.edu>
2024-06-25 16:43:53 -0400

    Related to GTR-13 - CreateClubModal creates clubs and adds them to the TestCollection

--- client/src/components/homepage/CreateClubModal.jsx
+++ client/src/components/homepage/CreateClubModal.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CreateClubModal.css";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../services/firebase"
 
 const CreateClubModal = ({ show, onClose }) => {
+    const [clubName, setClubName] = useState("");
+    const [clubDescription, setClubDescription] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await addDoc(collection(db, "TestCollection"), {
+                name: clubName,
+                description: clubDescription,
+                createdAt: new Date()
+            });
+            console.log("Document successfully created!")
+            onClose(); // close the modal after successful submission
+        } catch (error) {
+            console.log("Error creating document: ", error);
+        }
+    }
+
     if (!show) {
         return null;
     }
@@ -10,12 +30,12 @@ const CreateClubModal = ({ show, onClose }) => {
         <div className='create-club-modal__wrapper'>
             <div className='create-club-modal__content'>
                 <h2>Create a New Club</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className='create-club-modal__input-box'>
-                        <input id='club-name' type='text' placeholder='Club Name' required />
+                        <input id='club-name' type='text' placeholder='Club Name' value={clubName} onChange={(e) => setClubName(e.target.value)} required />
                     </div>
                     <div className='create-club-modal__input-box'>
-                        <textarea id='club-description' placeholder='Description'></textarea>
+                        <textarea id='club-description' placeholder='Description' value={clubDescription} onChange={(e) => setClubDescription(e.target.value)}></textarea>
                     </div>
                     <div className='create-club-modal__buttons'>
                         <button className='create-club-modal__cancel' type='button' onClick={onClose}>Cancel</button>
