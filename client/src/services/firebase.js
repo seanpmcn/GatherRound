61afb6381d6c720bd2041d9e15f99e6dde383b79
Liam Boston <lvb5483@psu.edu>
2024-06-25 16:42:11 -0400

    added firestore instance to firebase config

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
@@ -16,4 +17,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+// Initialize Auth & Firestore
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Export Auth & Firestore
+export { db, auth };
