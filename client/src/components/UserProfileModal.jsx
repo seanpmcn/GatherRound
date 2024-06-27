5b98441290ee1badc1b793d81d304160211b9fee
seanpmcn <46979553+seanpmcn@users.noreply.github.com>
2024-06-27 16:20:25 -0400

    GTR-151: Revert modal change

--- client/src/components/UserProfileModal.jsx
+++ client/src/components/UserProfileModal.jsx
@@ -1,12 +1,9 @@
 const UserProfileModal = ({name, email}) => {
-
-    const modalRef = useRef<HTMLDialogElement | null>(null);
-
     return (
-        <dialog ref={modalRef} data-testid='modal'>
+        <div data-testid='modal'>
             <h2 data-testid='name'>{name}</h2>
             <h2 data-testid='email'>{email}</h2>
-        </dialog>
+        </div>
     );
 }
 
